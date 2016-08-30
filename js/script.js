d7c9a1c8476a2c4b1f44ae3a441ef5fa5dddfc6e
Manukhova <olga.manukhova@gmail.com>
2016-08-30 19:05:15 +0300

    Декор 6

--- js/script.js
+++ js/script.js
@@ -1,28 +1,47 @@
  var link = document.querySelector(".btn__feedback");
- var popup = document.querySelector(".modal-content");
- var close = popup.querySelector(".modal-content-close");
- var overlay = document.querySelector(".modal-overlay");
- var overclose = overlay.querySelector(".modal-overlay-show");
- var slidebtn = document.querySelector(".slide-btn");
+ var popup = document.querySelector(".modal__content");
+ var close = popup.querySelector(".modal__content-close");
+ var overlay = document.querySelector(".modal__overlay");
+ var overclose = overlay.querySelector(".modal__overlay-show");
+ var slidebtn = document.querySelectorAll(".slide-btn");
  var basketlink = document.querySelector(".basket__link");
  var basketarea = document.querySelector(".basket__area");
 
+ var input = popup.querySelectorAll(".modal__input");
+ var label = popup.querySelectorAll(".label");
+
  link.addEventListener("click", function(event) {
         event.preventDefault();
-        popup.classList.add("modal-content-show");
-        overlay.classList.add("modal-overlay-show");
+        popup.classList.add("modal__content-show");
+        overlay.classList.add("modal__overlay-show");
       });
 
  close.addEventListener("click", function(event) {
         event.preventDefault();
-        popup.classList.remove("modal-content-show");
-        overlay.classList.remove("modal-overlay-show");
+        popup.classList.remove("modal__content-show");
+        overlay.classList.remove("modal__overlay-show");
       });
 
- slidebtn.addEventListener("click", function(event) {
-        event.preventDefault();
-        basketlink.classList.add("basket__link-show");
-        basketarea.classList.add("basket__area-goods");
-        basketarea.textContent = '2 товара';
+ for(var i = 0; i < slidebtn.length; i++){
+       slidebtn[i].addEventListener("click", function(event) {
+       event.preventDefault();
+       basketlink.classList.add("basket__link-show");
+       basketarea.classList.add("basket__area-goods");
+       basketarea.textContent = '2 товара';
+     });
+   }
 
-           });
+ for(var i = 0; i < input.length; i++){
+     input[i].addEventListener("blur",function(){
+       if(this.value.length > 0){
+          for(var i = 0; i < input.length; i++) {
+             label[i].classList.add("label-data");}
+
+     } else {
+         for(var i = 0; i < input.length; i++) {
+          label[i].classList.remove("label-data");
+        }
+     }
+  });
+
+    }
